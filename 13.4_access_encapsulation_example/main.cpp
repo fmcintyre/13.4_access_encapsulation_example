43f6481849a157ceb97cc370c0b062bb6463ebce
fmcintyre <finlay.mcintyre@gmail.com>
2022-11-03 13:49:16 +0000

    Initial Commit

---
+++ 13.4_access_encapsulation_example/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  13.4_access_encapsulation_example
+//
+//  Created by Finlay McIntyre on 03/11/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
